38429433b60c85613f956196523a83a22d12382a
Probalala <hodzic22@gmail.com>
2017-10-31 21:49:04 +0000

    Validations x2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-   validates :username, presence: true, 
+    has_many :articles
+    before_save { self.email = email.downcase }
+    validates :username, presence: true, 
         uniqueness: { case_sensitive: false }, 
         length: { minimum: 3, maximum: 20 } 
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
