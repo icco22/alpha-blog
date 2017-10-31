2c6e39a664e8cf5a950d01d2deb4cad27f3adccf
Probalala <hodzic22@gmail.com>
2017-10-31 22:14:35 +0000

    Informaciej o korisniku

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,5 @@ class Article < ActiveRecord::Base
     belongs_to :user
     validates :title, presence: true, length: { minimum: 3, maximum: 15 }   
     validates :description, presence: true, length: { minimum: 3, maximum: 50 }
+    validates :user_id, presence: true
 end
